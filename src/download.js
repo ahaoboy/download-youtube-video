8d9c3cd9ed704cbcff6f873ba0233143e03e5878
abcd <504595380@qq.com>
2020-02-28 12:53:35 +0800

    4. 7

--- src/download.js
+++ src/download.js
@@ -1,8 +1,22 @@
 // const ytdl = require('ytdl-core');
 const fs = require('fs')
 const axios = require('axios')
+const ytdl = require('ytdl-core');
 
 export async function  download(url, filePath) {
+
   let resp =await axios.get(url)
   fs.writeFileSync(filePath,resp.data,'utf8')
+  // let w = ytdl(url)
+  // let r = fs.createWriteStream(filePath)
+  // w.pipe(r);
+
+  // return new Promise(
+  //   resolve => {
+  //     w.on('close', () => {
+  //       resolve()
+  //     })
+  //   }
+  // )
+
 }
